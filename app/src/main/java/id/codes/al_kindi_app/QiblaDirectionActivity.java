cb398a192667349d1e388a9fcee7339bb5334ce6
stehanget <fahrizasirojudin@gmail.com>
2021-06-01 07:01:38 +0700

    add compass logic

--- app/src/main/java/id/codes/al_kindi_app/QiblaDirectionActivity.java
+++ app/src/main/java/id/codes/al_kindi_app/QiblaDirectionActivity.java
@@ -2,13 +2,57 @@ package id.codes.al_kindi_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class QiblaDirectionActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class QiblaDirectionActivity extends AppCompatActivity implements SensorEventListener {
+    @BindView(R.id.img_qibla_compass)
+    ImageView img_qibla_compass;
+
+    private static SensorManager sensorManager;
+    private static Sensor sensor;
+    private float currentDegree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qibla_direction);
+        ButterKnife.bind(this);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+
+        if (sensor != null) {
+            sensorManager.registerListener((SensorEventListener) this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+        } else {
+            Toast.makeText(this, "This device is not support", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int degree = Math.round(event.values[0]);
+        RotateAnimation animation = new RotateAnimation(currentDegree, -degree, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        animation.setDuration(500);
+        animation.setFillAfter(true);
+        img_qibla_compass.setAnimation(animation);
+        currentDegree = -degree;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
